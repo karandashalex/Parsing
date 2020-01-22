c66283eaa1bd63182c8b7f7aa2cce3cc36b418ed
Kurenev Alex <50239416+karandashalex@users.noreply.github.com>
2020-01-22 16:19:39 +0300

    Huge commit

--- Parsing.py
+++ Parsing.py
@@ -1,15 +1,31 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+# Main URL of website realt.by
+REALT_URL = 'https://realt.by/sale/flats/?view=0'
 
+
+# Open url and return html
 def get_html(url):
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     return urllib.request.urlopen(req).read()
 
 
+# Get html and return last page number
+def get_page_count(html):
+    soup = BeautifulSoup(html, features='html.parser')
+    pages = soup.find('div', class_='uni-paging')
+    last_page = pages.find_all('a')[-1].text
+    return int(last_page)
+
+
+# Get html and parse one page. Return flats list
 def parse(html):
+    # Get center table with flats
     soup = BeautifulSoup(html, features='html.parser')
     table = soup.find('div', class_='bd-table')
+
+    # Find in page all flats and append all of them in list
     header = table.find_all('div', class_='bd-table-item-header')
     flats = []
     for head in header:
@@ -25,15 +41,34 @@ def parse(html):
             'price': [''.join(pr.text.split()) for pr in head.find('div', class_='cena').find_all('span')]
             # 'price': [pr.text.strip().split() for pr in head.find('div', class_='cena').find_all('span')]
         })
-    for flat in flats:
-        print(flat)
+    return flats
 
 
 def main():
-    print('start')
-    html = get_html('https://realt.by/sale/flats/?view=0&page=0')
-    parse(html)
-    print('good work')
+    print('Start parsing. Wait...')
+
+    # Get html
+    html = get_html(REALT_URL)
+
+    # Get number of last page
+    page_count = get_page_count(html)
+    #page_count = 1
+    print('Find %d pages' % page_count)
+
+    # List with all flats
+    allflats = []
+
+    # Find flats on all pages
+    for page in range(0, page_count):
+        print('Parsing %d%%' % (page / page_count * 100))
+        html = get_html(REALT_URL + '&page=%d' % page)
+        allflats.extend(parse(html))
+
+    print('Parsing finished')
+
+    # Print list
+    for flat in allflats:
+        print(flat)
 
 
 if __name__ == '__main__':
--- README.md
+++
@@ -1,2 +0,0 @@
-# ParsingProject
- Parsing realt.by
