82f7c6d2c74dae6744feedb708e269d4e7366abe
Kurenev Alex <50239416+karandashalex@users.noreply.github.com>
2020-01-30 10:16:05 +0300

    Improved parsing

--- Parsing.py
+++ Parsing.py
@@ -30,16 +30,53 @@ def parse(html):
     flats = []
     for head in header:
         pl = head.find_all('div', class_='pl')
+        address = head.find('div', class_='ad').a.text.split(',')
+        floors = head.find('div', class_='ee').span.text.split('/')
+        max_floor = floors[1].strip().split(' ')[0]
+        try:
+            house_type = floors[1].strip().split(' ')[1]
+        except:
+            house_type = ''
+
+        sity = ''
+        street = ''
+        house = ''
+        total_price = ''
+        price = ''
+        if len(address):
+            sity = address.pop(0).strip()
+        if len(address):
+            street = address.pop(0).strip()
+        if len(address):
+            house = address.pop(0).strip()
+        prlist = [''.join(pr.text.split()) for pr in head.find('div', class_='cena').find_all('span')]
+        for pr in prlist:
+            if pr.find('руб/кв') > 0:
+                price = pr[:pr.find('руб/кв')]
+                if price.find('млн') > 0:
+                    price = int(float(price[:price.find('млн')].replace(',', '.')) * 1000000)
+            elif pr.find('руб'):
+                total_price = pr[:pr.find('руб')]
+                if total_price.find('млн') > 0:
+                    total_price = int(float(total_price[:total_price.find('млн')].replace(',', '.')) * 1000000)
+
         flats.append({
-            'c': head.find('div', class_='kv').span.text.strip(),
+            'rooms': head.find('div', class_='kv').span.text.strip().split('/')[0],
+            'separate_rooms': head.find('div', class_='kv').span.text.strip().split('/')[1],
             'area': head.find('div', class_='ra').span.text.strip(),
-            'address': head.find('div', class_='ad').a.text.strip(),
-            'floor': head.find('div', class_='ee').span.text.strip(),
-            'square': pl[0].span.text.strip(),
+            'sity': sity,
+            'street': street,
+            'house': house,
+            'floor': floors[0].strip(),
+            'max_floor': max_floor,
+            'house_type': house_type,
+            'total_square': pl[0].span.text.strip().split('/')[0],
+            'live_square': pl[0].span.text.strip().split('/')[1],
+            'kitchen_square': pl[0].span.text.strip().split('/')[2],
             'year': pl[1].span.text.strip(),
             'balcony': pl[2].span.text.strip(),
-            'price': [''.join(pr.text.split()) for pr in head.find('div', class_='cena').find_all('span')]
-            # 'price': [pr.text.strip().split() for pr in head.find('div', class_='cena').find_all('span')]
+            'total_price': total_price,
+            'price': price
         })
     return flats
 
@@ -51,8 +88,8 @@ def main():
     html = get_html(REALT_URL)
 
     # Get number of last page
-    #page_count = get_page_count(html)
-    page_count = 1
+    page_count = get_page_count(html)
+    #page_count = 1
     print('Find %d pages' % page_count)
 
     # List with all flats
